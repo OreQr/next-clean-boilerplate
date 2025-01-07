5c2d31081b775b3841928d655747ce88bc11998c
OreQr <50458706+OreQr@users.noreply.github.com>
2025-01-07 14:47:45 +0100

    Ref tailwindcss plugins

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss"
+import tailwindcssAnimate from "tailwindcss-animate"
 import { fontFamily } from "tailwindcss/defaultTheme"
 
 export default {
@@ -63,5 +64,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 } satisfies Config
