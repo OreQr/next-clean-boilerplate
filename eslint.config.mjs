133a56e0ebc5e6bf140184e62ace3e7d1366be85
OreQr <50458706+OreQr@users.noreply.github.com>
2025-04-27 21:50:57 +0200

    Ref ESLint config based on T3 stack

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,19 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-
-import nextPlugin from "@next/eslint-plugin-next"
-import tseslint from "@typescript-eslint/eslint-plugin"
-import tsParser from "@typescript-eslint/parser"
+import { FlatCompat } from "@eslint/eslintrc"
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 import tailwind from "eslint-plugin-tailwindcss"
+import tseslint from "typescript-eslint"
 
-/** @type {import("eslint").Linter.Config[]} */
-const eslintConfig = [
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+})
+
+export default tseslint.config(
   {
-    plugins: {
-      "@next/next": nextPlugin,
-    },
-    rules: {
-      ...nextPlugin.configs.recommended.rules,
-      ...nextPlugin.configs["core-web-vitals"].rules,
-    },
-    files: ["**/*.{mjs,ts,tsx}"],
+    ignores: [".next"],
   },
+  ...compat.extends("next/core-web-vitals"),
 
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
   ...tailwind.configs["flat/recommended"],
   {
     settings: {
@@ -28,61 +22,42 @@ const eslintConfig = [
       },
     },
   },
-
   {
-    plugins: {
-      "@typescript-eslint": tseslint,
-    },
-
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        project: "tsconfig.json",
-      },
-    },
-
+    files: ["**/*.{mjs,ts,tsx}"],
+    extends: [
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
     rules: {
-      ...tseslint.configs["recommended-type-checked"].rules,
-      ...tseslint.configs["stylistic-type-checked"].rules,
-
       "@next/next/no-html-link-for-pages": "off",
       "@next/next/no-img-element": "off",
       "tailwindcss/no-custom-classname": "off",
       "@typescript-eslint/array-type": "off",
       "@typescript-eslint/consistent-type-definitions": "off",
-
       "@typescript-eslint/consistent-type-imports": [
         "warn",
-        {
-          prefer: "type-imports",
-          fixStyle: "inline-type-imports",
-        },
+        { prefer: "type-imports", fixStyle: "inline-type-imports" },
       ],
-
       "@typescript-eslint/no-unused-vars": [
         "warn",
-        {
-          argsIgnorePattern: "^_",
-          ignoreRestSiblings: true,
-        },
+        { argsIgnorePattern: "^_", ignoreRestSiblings: true },
       ],
-
       "@typescript-eslint/require-await": "off",
-
       "@typescript-eslint/no-misused-promises": [
         "error",
-        {
-          checksVoidReturn: {
-            attributes: false,
-          },
-        },
+        { checksVoidReturn: { attributes: false } },
       ],
     },
   },
   {
-    ignores: [".next"],
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    },
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
   },
-  eslintPluginPrettierRecommended,
-]
-
-export default eslintConfig
+  eslintPluginPrettierRecommended
+)
